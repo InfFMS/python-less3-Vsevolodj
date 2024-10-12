33b2d4c29ee5a010b24cbc4ef81e319dffa73783
Vsevolod <v.ogorodnikov1@shool.nsu.ru>
2024-10-12 22:18:21 +0700

    Сделал задачу 2

--- task2.py
+++ task2.py
@@ -1,4 +1,16 @@
 # с клавиатуры вводятся числа, ввод завершается числом 0.
 # Определить, сколько было введено двузначных натуральных чисел,
 # и сколько других.
+count_poz = 0
+count_dr = 0
+value = int(input())
+while value !=0:
+    if value>0 and 9<value<100:
+        count_poz+=1
+    else:
+        count_dr+=1
+    value = int(input())
+print("Положительных двузначных:",count_poz, "Других:",count_dr )
+
+
 
