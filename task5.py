bc84099c68bd866b91f32af8b9f03e7eaea8a2d3
Vsevolod <v.ogorodnikov1@shool.nsu.ru>
2024-10-14 14:17:54 +0700

    Сделал задачу 5

--- task5.py
+++ task5.py
@@ -2,15 +2,22 @@
 # Определить, среднее арифметическое тех введённых чисел,
 # которые являются степенями числа 2.
 # Вывести "нет", если таких чисел нет.
+count=0
+sum = 0
+val = int(input())
 
-value = int(input())
-while value !=0:
-    value = value/2
-    if value%2==0:
-        sum += value
+while val !=0:
+    value = val
+    while value != 0:
+        value = value/2
+        if value%2==1:
+            break
+    if value==1:
+        sum= sum+val
+        count +=1
 
-    value = int(input())
-print("Сумма:",sum)
+    val = int(input())
+print("Cреднее:",sum/count)
 
 
 
