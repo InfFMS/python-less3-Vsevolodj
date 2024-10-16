36c59d5a1ad89c82fb0163aa5d70b2ccd59fe1a9
Vsevolod <v.ogorodnikov1@shool.nsu.ru>
2024-10-16 17:47:22 +0700

    Сделал задачу 10

--- task10.py
+++ task10.py
@@ -2,6 +2,26 @@
 # Определить минимальное и максимальное среди простых чисел
 # (которые делятся на сами не себя и на 1).
 # Если таких чисел не было, вывести "нет".
+def isPrime(n):
+    for i in range(2,n+1):
+        if n%i==0 and n!=i:
+            return 0
+            break
+        if n%i==0 and n==i:
+            return 1
+            break
+    return 0
 
 
+
+s=[]
+val = int(input())
+while val !=0:
+    value = int(input())
+    if isPrime(value) == 1:
+        s +=[value]
+    val-=1
+if len(s)!=0:
+    print(f'Минимальное:{min(s)},Максимальное:{max(s)}')
+else: print("нет")
 
